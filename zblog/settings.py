250ed5e06100145595909d00e273eed227fd1038
郑廷宇 <1050363373@qq.com>
2020-01-28 18:44:43 +0800

    修改数据库配置

--- zblog/settings.py
+++ zblog/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':"blog_zty",
         'USER':'root',
-        'PASSWORD':'31862063',
+        'PASSWORD':'while(0725)ztyLogin[Mysql]++1',
         'HOST':'localhost',
         'PORT':3306,
     }
@@ -124,5 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static_collect')
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
