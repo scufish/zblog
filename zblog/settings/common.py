a2c3173b35c3db13a12909459e28b0843d11fea8
郑廷宇 <1050363373@qq.com>
2020-01-29 21:24:23 +0800

    将配置文件拆分，便于生产环境和开发环境

--- zblog/settings/common.py
+++ zblog/settings/common.py
@@ -13,19 +13,19 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lf(_qlu)tg_ely_6x2xel9ymlx1=dg*zvl53ism%v(b0h=030-'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zhengtingyu.top']
+
+
 
 
 # Application definition
@@ -124,5 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'static_collect')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
